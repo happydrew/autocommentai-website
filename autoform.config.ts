ae149924c13ea3cb3d636bf0d3547dbff22ab3a9
zhugechina <zhugetd@126.com>
2025-06-18 00:05:20 +0800

    add payment page

--- autoform.config.ts
+++ autoform.config.ts
@@ -13,5 +13,5 @@ const RESET_PASS_URL = `${backEnd}/api/auth/reset-password/reset-pass`;
 const GIVE_FREE_CREDITS_URL = `${backEnd}/api/payment/giveFreeCredits`;
 const CONCAT_URL = `${backEnd}/api/concat`;
 
-//const QUERY_ORDER_STATUS_URL = 'https://api-autocommentai.randompokegen.cc/api/payment/query-status';
-const QUERY_ORDER_STATUS_URL = 'http://localhost:3000/api/payment/query-status';
+const QUERY_ORDER_STATUS_URL = 'https://api-autocommentai.randompokegen.cc/api/payment/query-status';
+//const QUERY_ORDER_STATUS_URL = 'http://localhost:3000/api/payment/query-status';
