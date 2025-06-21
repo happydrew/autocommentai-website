3d680c1b558e54518e708b6fa6715e75f40d8b5e
zhugechina <zhugetd@126.com>
2025-06-21 13:40:25 +0800

    add demo vedio

--- components/homepage/GetExtensionButton.tsx
+++ components/homepage/GetExtensionButton.tsx
@@ -6,7 +6,7 @@ const GetExtensionButton = () => {
             <a className='flex items-center justify-center flex-nowrap whitespace-nowrap mt-2 px-6 py-3 rounded-full text-white bg-gradient-to-b from-[#d97706] to-[#92400e] [text-shadow:0_1px_1px_#00387838] [box-shadow:0_1px_2px_#00295738] transition-all duration-200 ease-in-out select-none [user-drag:none] [-webkit-tap-highlight-color:transparent]'
                 href="https://chromewebstore.google.com/detail/autocommentai/lnnbcbhcffnggkibgmhhjjgadjiainhd"
                 target="_blank">
-                Get Browser Extension
+                Add to Chrome/Edge
                 <ChromeIcon className="inline-block ml-2 w-8 h-8" />
                 <EdgeBrowserIcon className="inline-block ml-2 w-8 h-8" />
             </a>
